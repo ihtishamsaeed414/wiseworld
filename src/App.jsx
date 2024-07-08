aca41dfe023492e94115358dcf0f09230eabe364
Ihtisham Saeed <111792380+ihtishamsaeed414@users.noreply.github.com>
2024-07-08 22:34:41 +0500

    Routes and nested routes completed along with App navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,46 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:9000";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of cities</p>} />
           <Route path="form" element={<p>List of cities</p>} />
         </Route>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>map</div>;
+  return <div className={styles.mapContainer}></div>;
 }
 
 export default Map;
